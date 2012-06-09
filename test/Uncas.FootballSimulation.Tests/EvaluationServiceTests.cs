09db78b55506b392d085437392498ce99451ade5
Ole Lynge Soerensen <olelynge@gmail.com>
2012-06-09 10:17:27 +0200

    Add .NET solution for simulation.

---
+++ test/Uncas.FootballSimulation.Tests/EvaluationServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Uncas.FootballSimulation.Tests
+{
+    public class EvaluationService
+    {
+        public EvaluationResult EvaluateMatch()
+        {
+            return new EvaluationResult();
+        }
+    }
+
+    [TestFixture]
+    public class EvaluationServiceTests
+    {
+        private EvaluationService _evaluationService = new EvaluationService();
+
+        [Test]
+        public void EvaluateMatch()
+        {
+            EvaluationResult evaluationResult = _evaluationService.EvaluateMatch();
+
+            Assert.NotNull(evaluationResult);
+        }
+    }
+}
